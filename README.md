88ca8d4a33fa7da3b49fdb7641c1074ce29078cd
Kyle O'Neill <oneillkyle@gmail.com>
2016-08-16 23:58:37 -0400

    added buttons file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# raspberry-pi-test" 
+# raspberry-pi-test
---
+++ buttons.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18,GPIO.IN)
+GPIO.setup(21,GPIO.IN)
+GPIO.setup(12,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(12, GPIO.input(18))
+        GPIO.output(16, GPIO.input(21))
+except KeyboardInterrupt:
+    GPIO.cleanup()
